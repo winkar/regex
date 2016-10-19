3067e0b4fa68679b9e3f95ca2ffe639e1efde972
winkar <winkarddo@gmail.com>
2016-10-19 11:18:18 +0800

    Change: # Add clone package # Wrap global declare on String interface # Add inputStream interface # Remove NfaNode class # Add JSON support to Episilon class # export NFA class

--- regex.ts
+++ regex.ts
@@ -1,12 +1,13 @@
-interface inputStream {
-    read(size: number): string;
-}
+import clone = require("clone");
 
-interface String {
-    isWhiteSpace(): boolean
-    isAlpha(): boolean
-    isNum(): boolean
-    isAlphaNum(): boolean
+
+declare global {
+    interface String {
+        isWhiteSpace: () => boolean
+        isAlpha: () => boolean
+        isNum: () => boolean
+        isAlphaNum: () => boolean
+    }
 }
 
 String.prototype.isWhiteSpace = function () {
@@ -25,6 +26,11 @@ String.prototype.isAlphaNum = function () {
     return /[a-zA-Z0-9]/.test(this);
 }
 
+interface inputStream {
+    read(size: number): string;
+}
+
+
 
 class parser {
     in: inputStream
@@ -33,7 +39,7 @@ class parser {
     }
 
     lastNFA: NFA
-    peek: String
+    peek: string
 
     startBuildingNFA(): NFA {
         this.peek = this.in.read(1)
@@ -62,79 +68,114 @@ class parser {
             lhsNFA = NFA.kleenClosure(lhsNFA)
         }
 
-        return NFA.concatNFA(lhsNFA, this.buildNFA())
+        return NFA.concat(lhsNFA, this.buildNFA())
     }
 }
 
 /**
  * NFA 实现
  */
-class Episilon {}
-let episilon = new Episilon();
+class Episilon {
+    toJSON(): string {
+        return "ε";
+    }
+}
 
 type transferValue = string | Episilon
 
 class NfaEdge {
     expr: transferValue
-    from: NfaNode
-    to: NfaNode
-    constructor(from: NfaNode, to: NfaNode, expr?: transferValue) {
-        this.from = from
+    to: number
+    constructor(to: number, expr?: transferValue) {
         this.to = to
-        this.from.addOutEdge(this)
-        this.to.addInEdge(this)
         if (expr) {
             this.expr = expr
         }
     }
-}
 
-
-
-class NfaNode {
-    in: Array<NfaEdge>
-    out: Array<NfaEdge>
-
-    addOutEdge(edge: NfaEdge) {
-        this.out.push(edge)
-    }
-
-    addInEdge(edge: NfaEdge) {
-        this.in.push(edge)
+    toString(): string {
+        return JSON.stringify(this, null, 4)
     }
 }
 
-class NFA {
-    start: NfaNode
-    finish: NfaNode
 
-    constructor(s?: transferValue) {
+
+export default class NFA {
+    static episilon = new Episilon();
+    nodesNumber: number
+    start: number
+    finish: number
+    edgesFromNode: Array<Array<NfaEdge>>
+
+    constructor(nfa?: NFA, s?: transferValue) {
+        if (nfa) {
+            this.nodesNumber = nfa.nodesNumber
+            this.start = nfa.start
+            this.finish = nfa.finish
+            this.edgesFromNode = clone(nfa.edgesFromNode)
+        }
         if (s) {
-            this.start = new NfaNode()
-            this.finish = new NfaNode()
-            let edge = new NfaEdge(this.start, this.finish, s)
+            this.start = 0
+            this.finish = 1
+            this.nodesNumber = 2
+            this.edgesFromNode = new Array<Array<NfaEdge>>()
+            let edge = new NfaEdge(this.finish, s)
+            if (!this.edgesFromNode[this.start]) {
+                this.edgesFromNode[this.start] = new Array<NfaEdge>()
+            }
+            this.edgesFromNode[this.start].push(edge)
         }
     }
 
-    static copyNodeRecursive(node: NfaNode): NfaNode {
-        let newNode = new NfaNode();
-
+    toString(): string {
+        return JSON.stringify(this, null, 4)
     }
 
-    static concatNFA(lhsNFA: NFA, rhsNFA: NFA): NFA {
-        let newNfa = new NFA();
-        newNfa.start = lhsNFA.start
-        newNfa.finish = rhsNFA.finish
-        lhsNFA.finish = rhsNFA.start
-        return newNfa
+    addNode() {
+        return this.nodesNumber++
+    }
+    addEdge(from: number, to: number, expr?: transferValue) {
+        if (!this.edgesFromNode[from]) {
+            this.edgesFromNode[from] = new Array<NfaEdge>()
+        }
+        this.edgesFromNode[from].push(new NfaEdge(to, expr))
+    }
+
+    private extends(anotherNFA: NFA) {
+        let newEdgeArr = clone(anotherNFA.edgesFromNode)
+        for (var i = 0; i < anotherNFA.nodesNumber; ++i) {
+            if (newEdgeArr[i]) {
+                // newEdgeArr[i] = newEdgeArr[i].map(
+                //     edge => new NfaEdge(edge.to + this.nodesNumber-1, edge.expr)
+                // )
+                for (var j=0; j< newEdgeArr[i].length; ++j) {
+                    newEdgeArr[i][j] = new NfaEdge(newEdgeArr[i][j].to + this.nodesNumber-1, newEdgeArr[i][j].expr)
+                }
+            }
+
+        }
+        this.edgesFromNode = this.edgesFromNode.concat(newEdgeArr)
+        return this
+    }
+
+    static concat(lhsNFA: NFA, rhsNFA: NFA): NFA {
+        let newNfa = new NFA(lhsNFA);
+        return newNfa.extends(rhsNFA)
     }
 
     static kleenClosure(nfa: NFA): NFA {
-        let node_s = new NfaNode(), node_f= new NfaNode()
-        let edge1 = new NfaEdge(node_s, nfa.start, episilon)
-        let edge2 = new NfaEdge(nfa.finish, node_f, episilon)
-        let edge3 = new NfaEdge(node_s, node_f, episilon)
-        let edge4 = new NfaEdge(nfa.finish, nfa.start, episilon)
-        return nfa
+        let newNfa = new NFA(nfa);
+        let newNode1 = newNfa.addNode()
+        let newNode2 = newNfa.addNode()
+        let start = newNfa.start
+        let finish = newNfa.finish
+        newNfa.addEdge(newNode1, start, NFA.episilon)
+        newNfa.addEdge(finish, newNode2, NFA.episilon)
+        newNfa.addEdge(newNode1, newNode2, NFA.episilon)
+        newNfa.addEdge(finish, start, NFA.episilon)
+        newNfa.start = newNode1
+        newNfa.finish = newNode2
+        return newNfa
     }
 }
+
